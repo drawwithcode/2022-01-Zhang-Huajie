6919f98dbb669cb6ff973024386e8f69e331816f
Zhang Huajie <113454002+giorgiojjjj6@users.noreply.github.com>
2022-09-24 11:56:47 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,98 +1,29 @@
+const dr = [10, 20, 40];
+dr.push(10);
 function setup() {
-  createCanvas(1450, 750);
+  createCanvas(windowWidth, windowHeight);
   angleMode(DEGREES);
+  background("black");
 }
 
 function draw() {
-   background("smokewhite");
-  stroke("black");
-  strokeWeight(15);
-  let myColor = lerpColor(color("whitesmoke"), color("blue"), frameCount / 240);
-  let myColor1 = lerpColor(color("whitesmoke"), color("red"), frameCount / 240);
-  let myColor2 = lerpColor(
-    color("whitesmoke"),
-    color("yellow"),
-    frameCount / 240
-  );
-
-  fill(myColor2);
-  circle(900, 400, 2100);
-  fill("whitesmoke");
-  circle(900, 400, 1900);
-  fill(myColor);
-  circle(900, 400, 1700);
-  fill(myColor1);
-  circle(900, 400, 1500);
-  fill(myColor2);
-  circle(900, 400, 1300);
-  fill("whitesmoke");
-  circle(900, 400, 1100);
-
-  stroke("black");
-  strokeWeight(10);
-
-  fill("whitesmoke");
-  circle(1400, 10, 300);
-  fill(myColor);
-  circle(1400, 10, 200);
-  fill(myColor1);
-  circle(1400, 10, 100);
-  fill(myColor2);
-  circle(1400, 10, 20);
-
-  fill("whitesmoke");
-  circle(100, 650, 400);
-  fill(myColor);
-  circle(100, 650, 300);
-  fill(myColor1);
-  circle(100, 650, 200);
-  fill(myColor2);
-  circle(100, 650, 100);
-  fill("whitesmoke");
-  circle(100, 650, 30);
-
-  fill("whitesmoke");
-  circle(400, 100, 550);
-  fill(myColor);
-  circle(400, 100, 425);
-  fill(myColor1);
-  circle(400, 100, 300);
-  fill(myColor2);
-  circle(400, 100, 175);
-  fill("whitesmoke");
-  circle(400, 100, 75);
-
-  stroke("black");
-  strokeWeight(15);
-
-  fill(myColor);
-  circle(900, 400, 900);
-  fill(myColor1);
-  circle(900, 400, 675);
-  fill(myColor2);
-  circle(900, 400, 450);
-  fill("whitesmoke");
-  circle(900, 400, 250);
-
-  stroke("black");
-  strokeWeight(4);
-  fill("red");
-  translate(width / 1.65, height / 2.5);
-  rotate(frameCount * 3);
-  circle(100, 70, 100);
-
-  fill("blue");
-  translate(60, 60);
-  rotate(frameCount * 1);
-  circle(150, 10, 100);
-
-  fill("yellow");
-  translate(120, 120);
-  rotate(frameCount * 1);
-  circle(50, 50, 100);
-
-  fill("whitesmoke");
-  translate(200, 200);
-  rotate(frameCount * 1);
-  circle(40, 40, 100);
+  translate(-5, -7.5);
+  frameRate(10);
+  const inc = 25;
+  for (let y = inc; y <= height; y += inc) {
+    for (let x = inc; x <= width; x += inc) {
+      const diameter = random(dr);
+      const randomValue = random(-10, 10);
+      if (randomValue < 1) {
+        const r = random(0, 255);
+        const g = random(0, 255);
+        const b = random(0, 255);
+        fill(r, g, b);
+      } else {
+        fill("white");
+      }
+      noStroke();
+      ellipse(x, y, diameter);
+    }
+  }
 }
