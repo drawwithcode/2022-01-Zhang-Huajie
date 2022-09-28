27fe1f00ebac9c6fd1bc1f689aae2c56154d63ce
Zhang Huajie <113454002+giorgiojjjj6@users.noreply.github.com>
2022-09-28 12:40:44 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,30 +1,29 @@
-const dr = [10, 20, 30];
+const dr = [20, 30, 40];
 dr.push(10);
-let noiseVal;
-let noiseScale = 0.02;
+let noiseScale = 0.01;
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  noiseSeed(99);
-  stroke(0, 10);
-  background("white");
+  noiseSeed(50);
+  stroke(0);
+  background("black");
 }
 
 function draw() {
+  frameRate(20);
   for (let y1 = 0; y1 < height; y1++) {
     for (let x1 = 0; x1 < width; x1++) {
-      noiseDetail(2, 0.2);
+      noiseDetail(10, 0.2);
       noiseVal = noise(x1 * noiseScale, y1 * noiseScale);
       stroke(noiseVal * 255);
       point(x1, y1);
     }
   }
-  frameRate(150);
-  const inc = 50;
+  const inc = 100;
   for (let y = inc; y < height; y += inc) {
     for (let x = inc; x < width; x += inc) {
       const diameter = random(dr);
-      const randomValue = random(-10, 10);
-      if (randomValue < 1) {
+      const randomValue = random();
+      if (randomValue < 0.3) {
         const r = random(0, 255);
         const g = random(0, 255);
         const b = random(0, 255);
