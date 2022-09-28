56863c7fcddb5776a3109ee9d24570b90f2919f0
Zhang Huajie <113454002+giorgiojjjj6@users.noreply.github.com>
2022-09-28 11:55:51 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,17 +1,27 @@
 const dr = [10, 20, 30];
 dr.push(10);
+let noiseVal;
+let noiseScale = 0.02;
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  angleMode(DEGREES);
-  background("black");
+  noiseSeed(99);
+  stroke(0, 10);
+  background("white");
 }
 
 function draw() {
-  translate(-10, -10);
-  frameRate(10);
-  const inc = 30;
-  for (let y = inc; y <= height; y += inc) {
-    for (let x = inc; x <= width; x += inc) {
+  for (let y1 = 0; y1 < height; y1++) {
+    for (let x1 = 0; x1 < width; x1++) {
+      noiseDetail(2, 0.2);
+      noiseVal = noise(x1 * noiseScale, y1 * noiseScale);
+      stroke(noiseVal * 255);
+      point(x1, y1);
+    }
+  }
+  frameRate(150);
+  const inc = 50;
+  for (let y = inc; y < height; y += inc) {
+    for (let x = inc; x < width; x += inc) {
       const diameter = random(dr);
       const randomValue = random(-10, 10);
       if (randomValue < 1) {
@@ -23,7 +33,7 @@ function draw() {
         fill("white");
       }
       noStroke();
-      ellipse(x, y, diameter);
+      ellipse(random(x), random(y), diameter);
     }
   }
 }
